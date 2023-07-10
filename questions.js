deb1cb48425099ff5f11bd47231da294c2688a58
Rahul Gaba <61040825+Rahul-Gaba@users.noreply.github.com>
2023-07-10 11:15:10 -0700

    Add files via upload
    This project is using html, Css and javascript We will create the structure using HTML and design it with CSS. 
    JavaScript will be used to implement the logic and scoring of the quiz.
    features of this project are:-
    1.The questions will be displayed one by one with their options.
    2.We have to choose the answer from the given options. Only by selecting the option will display whether the selected option is correct or not.
    3.We cannot proceed to the next question without choosing an option. I mean next button will not appear until we select any option.
    4.The total number of questions will be shown in the quiz, along with which question you have reached, it will also be shown.
    5.When you reach the last question, the finish button will appear.
    6.And after press the finish button , the result will be shown can be correct or incorrect answer.

---
+++ questions.js
@@ -0,0 +1,83 @@
+var questions = [{
+    num: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Multiple Language",
+      "Hyper Text Preprocessor",
+      "Hyper Tool Multi Language",
+      "Hyper Text Markup Language"
+    ]
+  },
+  {
+    num: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Computer Style Sheet",
+      "Cascading Style Sheet",
+      "Colorful Style Sheet",
+      "Common Style Sheet"
+    ]
+  },
+  {
+    num: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hometext Preprocessor",
+      "Hypertext Preprogramming"
+    ]
+  },
+
+  {
+    num: 4,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXTra Multi-Program Language",
+      "eXecutable Multiple Language",
+      "eXtensible Markup Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+    num: 5,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Statement Question Language",
+      "Stylesheet Query Language",
+      "Stylish Question Language",
+      "Structured Query Language"
+    ]
+  },
+
+  {
+    num: 6,
+    question: "Javascript is an _______ language?",
+    answer: "Object Oriented",
+    options: [
+      "Object Oriented",
+      "object Based",
+      "Procedural",
+      "None of the above"
+    ]
+  },
+
+  {
+    num: 7,
+    question: "Which of the following methods is used to access HTML elements using Javascript?",
+    answer: "Both A and B",
+    options: [
+      "getElementbyId()",
+      "getElementsByClassName()",
+      "Both A and B",
+      "None of the above"
+    ]
+  },
+
+
+];
